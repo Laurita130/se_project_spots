fd72a18d417b03912f1f233c0e1a0d7282ec381b
Laura Alburez <lauraavidle17@icloud.com>
2025-05-22 20:56:51 -0400

    java script

--- scripts/index.js
+++ scripts/index.js
@@ -1,12 +1,27 @@
 const editProfileBtn = document.querySelector(".profile__edit-btn");
 const editProfileModal = document.querySelector("#edit-profile-modal");
 const editProfileCloseBtn = editProfileModal.querySelector(".modal__close-btn");
+const editProfileForm = editProfileModal.querySelector(".modal__form");
+const editProfileNameInput = editProfileModal.querySelector(
+  "#profile-name-input"
+);
+const editProfileDescriptionInput = editProfileModal.querySelector(
+  "#profile-description-input"
+);
+const addCardFormElement = document.querySelector(".modal__form");
+const nameInput = document.querySelector("#profile-name-input");
+const linkInput = document.querySelector(".card-image-input");
 
 const newPostBtn = document.querySelector(".profile__add-btn");
 const newPostModal = document.querySelector("#new-post-modal");
 const newPostCloseBtn = newPostModal.querySelector(".modal__close-btn");
 
+const profileNameEl = document.querySelector(".profile__name");
+const profileDescriptionEl = document.querySelector(".profile__description");
+
 editProfileBtn.addEventListener("click", function () {
+  editProfileNameInput.value = profileNameEl.textContent;
+  editProfileDescriptionInput.value = profileDescriptionEl.textContent;
   editProfileModal.classList.add("modal_is-opened");
 });
 
@@ -21,3 +36,27 @@ newPostBtn.addEventListener("click", function () {
 newPostCloseBtn.addEventListener("click", function () {
   newPostModal.classList.remove("modal_is-opened");
 });
+
+function handleEditProfileSubmit(evt) {
+  evt.preventDefault();
+  profileNameEl.value = editProfileNameInput.textContent.value;
+  profileDescriptionEl.value = editProfileDescriptionInput.textContent.value;
+  editProfileModal.classList.remove("modal_is-opened");
+}
+function handleAddCardSubmit(evt) {
+  // Prevent default browser behavior.
+  evt.preventDefault();
+}
+
+function openModal() {
+  modal.classList.remove("hidden");
+  setTimeout(() => modal.classList.add("show"), 10);
+}
+
+function closeModal() {
+  modal.classList.remove("show");
+  setTimeout(() => modal.classList.add("hidden"), 10);
+}
+addCardFormElement.addEventListener("submit", handleAddCardSubmit);
+
+editProfileForm.addEventListener("submit", handleEditProfileSubmit);
