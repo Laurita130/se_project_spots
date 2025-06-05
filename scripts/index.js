e90745bed700c3dee22fefc3ee9975070b0dafd5
Laura Alburez <lauraavidle17@icloud.com>
2025-06-04 21:32:00 -0400

    changes

--- scripts/index.js
+++ scripts/index.js
@@ -9,8 +9,6 @@ const editProfileDescriptionInput = editProfileModal.querySelector(
   "#profile-description-input"
 );
 
-const nameInput = document.querySelector("#profile-name-input");
-
 const newPostModal = document.querySelector("#new-post-modal");
 const linkInput = newPostModal.querySelector("#card-image-input");
 const captionInput = newPostModal.querySelector("#card-caption-input");
@@ -52,6 +50,12 @@ function handleAddCardSubmit(evt) {
   evt.preventDefault();
   console.log("linkInput", linkInput.value);
   console.log("captionInput", captionInput.value);
+}
+
+function closeModal() {
+  console.log("Modal Closed");
+  document.linkInput("myModal").style.display = "none";
+  document.captionInput("myModal").style.display = "none";
 }
 
 addCardFormElement.addEventListener("submit", handleAddCardSubmit);
